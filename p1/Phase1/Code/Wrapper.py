e531d59956143017c9cc43693bd35e9806b42a20
Dylan Fox <dylfox21@gmail.com>
2020-02-06 17:22:07 -0800

    corner detection

--- p1/Phase1/Code/Wrapper.py
+++ p1/Phase1/Code/Wrapper.py
@@ -19,26 +19,82 @@ University of Maryland, College Park
 
 import numpy as np
 import cv2
+import argparse
+import os
 # Add any python libraries here
 
 
 
 def main():
 	# Add any Command Line arguments here
-    # Parser = argparse.ArgumentParser()
-    # Parser.add_argument('--NumFeatures', default=100, help='Number of best features to extract from each image, Default:100')
-    
-    # Args = Parser.parse_args()
-    # NumFeatures = Args.NumFeatures
+	# Parser = argparse.ArgumentParser()
+	# Parser.add_argument('--NumFeatures', default=100, help='Number of best features to extract from each image, Default:100')
+	
+	# Args = Parser.parse_args()
+	# NumFeatures = Args.NumFeatures
+
+	Parser = argparse.ArgumentParser()
+	Parser.add_argument('--ImageSetBasePath', default="../Data/Train/Set1", help='Number of best features to extract from each image, Default: ../Data/Train/Set1')
+	Parser.add_argument('--NumFeatures', default=100, help='Number of best features to extract from each image, Default:100')
+
+	Args = Parser.parse_args()
+	NumFeatures = Args.NumFeatures
+	ImageSetBasePath = Args.ImageSetBasePath
+
 
 	"""
 	Read a set of images for Panorama stitching
 	"""
 
+	images = []
+
+	for file in os.listdir(ImageSetBasePath):
+		if(file.endswith(".png") or file.endswith(".jpg")):
+			file = ImageSetBasePath + "/" +  file
+			images.append(cv2.imread(file,cv2.IMREAD_GRAYSCALE))
+		else:
+			continue
+
+
+	for corner in images:
+		cv2.imshow("d", corner)
+		cv2.waitKey(0)
+
+
 	"""
 	Corner Detection
 	Save Corner detection output as corners.png
 	"""
+
+	corners = []
+
+	for img in images:
+		img32 = np.float32(img)
+		dst = cv2.cornerHarris(img32, 2,3,0.04)
+
+		#dst = cv2.dilate(dst, None, iterations=1)
+
+		dst[dst<100000] = 0
+
+		print(dst + img)
+
+		print(img)
+		print(img)
+
+		cv2.imshow("d", img)
+		cv2.waitKey(0)
+		print(dst)
+		cv2.imshow("d", dst)
+		cv2.waitKey(0)
+
+
+	for corner in corners:
+		print(corner)
+		cv2.imshow("d", corner)
+		cv2.waitKey(0)
+
+
+
 
 	"""
 	Perform ANMS: Adaptive Non-Maximal Suppression
@@ -66,7 +122,6 @@ def main():
 	Save Panorama output as mypano.png
 	"""
 
-    
 if __name__ == '__main__':
-    main()
+	main()
  
