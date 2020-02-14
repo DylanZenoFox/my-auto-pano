5c09d2348e53e57ff524d69b95065969fe17bec4
Dylan Fox <dylfox21@gmail.com>
2020-02-13 22:01:40 -0800

    staching for git pull

--- p1/Phase1/Code/Wrapper.py
+++ p1/Phase1/Code/Wrapper.py
@@ -34,7 +34,7 @@ def main():
 	# NumFeatures = Args.NumFeatures
 
 	Parser = argparse.ArgumentParser()
-	Parser.add_argument('--ImageSetBasePath', default="../Data/Train/Set1", help='Number of best features to extract from each image, Default: ../Data/Train/Set1')
+	Parser.add_argument('--ImageSetBasePath', default="../Data/Train/Set3", help='Number of best features to extract from each image, Default: ../Data/Train/Set1')
 	Parser.add_argument('--NumFeatures', default=100, help='Number of best features to extract from each image, Default:100')
 
 	Args = Parser.parse_args()
@@ -100,6 +100,8 @@ def main():
 	Perform ANMS: Adaptive Non-Maximal Suppression
 	Save ANMS output as anms.png
 	"""
+
+	
 
 	"""
 	Feature Descriptors
